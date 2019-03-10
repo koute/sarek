a5979b5155dbbbade1438f3f855bc343067ebd76
Jan Bujak <j@exia.io>
2019-03-10 21:01:31 +0100

    Try to fix Travis: use `python3.7` directly

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,8 +5,8 @@ IFS=$'\n\t'
 
 export RUST_BACKTRACE=1
 
-export PYTHON_SYS_EXECUTABLE=python3
-echo "Python version: `python3 --version`"
+export PYTHON_SYS_EXECUTABLE=python3.7
+echo "Python version: `python3.7 --version`"
 
 cargo build
 cargo build --example mnist
