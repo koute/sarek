97d56d667c7134645af2e67dbe6b687c19568502
Jan Bujak <j@exia.io>
2019-04-27 14:58:07 +0200

    Ensure TensorFlow's output is deterministic in debug mode

--- src/backend/keras/context.rs
+++ src/backend/keras/context.rs
@@ -5,7 +5,10 @@ use {
     lazy_static::lazy_static,
     log::info,
     pyo3::{
-        prelude::*
+        prelude::*,
+        types::{
+            PyDict
+        }
     },
     crate::{
         backend::{
@@ -39,26 +42,55 @@ impl Context {
         let version: String = sys.get( "version" )?.extract()?;
         info!( "Using Python {}", version.replace( "\n", "" ) );
 
-        let tf = py.import( "tensorflow" )?;
-        let tf_version: String = tf.get( "VERSION" )?.extract()?;
-        info!( "Using TensorFlow {}", tf_version );
+        py.import( "random" )?.getattr( "seed" )?.call( (1234,), None )?;
 
         let np = py.import( "numpy" )?;
         let np_version: String = np.get( "version" )?.getattr( "version" )?.extract()?;
         info!( "Using numpy {}", np_version );
 
+        np.getattr( "random" )?.getattr( "seed" )?.call( (1234,), None )?;
+
+        let tf = py.import( "tensorflow" )?;
+        let tf_version: String = tf.get( "VERSION" )?.extract()?;
+        info!( "Using TensorFlow {}", tf_version );
+
         let logging = tf.getattr( "logging" )?;
         let log_level = logging.getattr( "ERROR" )?;
         logging.getattr( "set_verbosity" )?.call( (log_level,), None )?;
+
+        tf.getattr( "set_random_seed" )?.call( (1234,), None )?;
+
+        if cfg!( debug_assertions ) {
+            info!( "Disabling parallelism to ensure determinism" );
+
+            let kwargs = PyDict::new( py );
+            kwargs.set_item( "intra_op_parallelism_threads", 1 )?;
+            kwargs.set_item( "inter_op_parallelism_threads", 1 )?;
+            let session_conf = tf.getattr( "ConfigProto" )?.call( (), Some( kwargs ) )?;
+            let default_graph = tf.getattr( "get_default_graph" )?.call( (), None )?;
+            let kwargs = PyDict::new( py );
+            kwargs.set_item( "graph", default_graph )?;
+            kwargs.set_item( "config", session_conf )?;
+            let session = tf.getattr( "Session" )?.call( (), Some( kwargs ) )?;
+            tf.getattr( "keras" )?.getattr( "backend" )?.getattr( "set_session" )?.call( (session,), None )?;
+        }
 
         let ctx = Context { _dummy: () };
         Ok( ctx )
     }
 
     pub fn new() -> Result< Self, ContextCreationError > {
-        Context::gil( |py| {
-            Self::new_internal( py ).map_err( |err| py_err( py, err ) ).map_err( ContextCreationError )
-        })
+        lazy_static! {
+            static ref CONTEXT: Result< Context, ContextCreationError > = {
+                env::set_var( "PYTHONHASHSEED", "0" );
+
+                Context::gil( |py| {
+                    Context::new_internal( py ).map_err( |err| py_err( py, err ) ).map_err( ContextCreationError )
+                })
+            };
+        }
+
+        CONTEXT.clone()
     }
 
     pub(crate) fn gil< R, F >( callback: F ) -> R where F: FnOnce( Python ) -> R + Send, R: Send + 'static {
